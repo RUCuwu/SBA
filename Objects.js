a95441ce41747f00b160e0cb7089df28730836e3
RUCuwu <afiji.x04@gmail.com>
2024-08-10 07:54:38 -0500

    Add files via upload

---
+++ Objects.js
@@ -0,0 +1,34 @@
+let Hello = "Hey Ya'll";
+console.log(Hello)
+
+let countries = new Array('Italy', 'Germany', 'Japan', 'South Korea');
+const car = Array('Lamborghini ', 'Volkswagen ', 'Infiniti ', 'Hyundai ');
+
+for (let i = 0; i < countries.length; i++) {
+    console.log(countries[i]);
+}
+
+// let Best = countries.concat();
+
+// console.log(Best);
+
+// car.push('Trading Places', 'Antitrust');
+
+Array[Array.length] = countries;
+
+let countriesString = car.join( 'or ');
+
+let countriesArr= countriesString.split(' . ')
+
+console.log('Either' , countriesString + "are the best.");
+
+//car and locations
+
+function gatherdata() {
+    let numbers = [];
+    for (let i = i;
+        userInput = parseFloat(prompt('1'));
+    numbers.push(userInput);)
+}
+return numbers;
+
